42cad57e3f2ba23f65f5a40c760d9749a1a603e0
THJepTheDep <84139397+THJepTheDep@users.noreply.github.com>
2023-01-28 13:17:39 +0100

    Add files via upload

--- DesktopOS.py
+++ DesktopOS.py
@@ -1,7 +1,17 @@
 import os
 import time as tm
+import tkinter as tk
 
 os.system('cls')
 tm.sleep(0.2)
-print("This is the desktop version of the OS")
-input()
+print("Initializing..")
+
+window = tk.Tk()
+
+window.attributes('-fullscreen', True)
+window.title("MazOS Desktop v0.1.2")
+
+label = tk.Label(window, text="Maz OS Desktop Version")
+label.pack()
+
+window.mainloop()
